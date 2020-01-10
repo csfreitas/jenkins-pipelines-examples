9dd8e7caccdad6606cf5d35ef7e2796991a3bba8
Cassio Augusto Silva de Freitas <kssiofreitas@gmail.com>
2020-01-09 22:05:39 -0300

    Ajustes template

--- ocp-files-examples/producao/setup_prod.sh
+++ ocp-files-examples/producao/setup_prod.sh
@@ -2,17 +2,29 @@
 
 DEV_PROJECT=$1
 PROJECT=$2
+
+if [ -z "$DEV_PROJECT" ]
+  then
+  	echo "DEV_PROJECT o comprimento é zero -  informar parametro $1"
+  	echo "Exiting..."
+  	exit 0
+  elif [ -z "$PROJECT" ];
+  then
+  	echo "PROJECT o comprimento é zero -  informar parametro $2"
+  	echo "Exiting..."
+  	exit 0
+fi
 # Set up Production Project
 oc policy add-role-to-group system:image-puller system:serviceaccounts:${PROJECT} -n ${DEV_PROJECT}
 oc policy add-role-to-user edit system:serviceaccount:cicd:jenkins -n ${PROJECT}
 
 # Set up Blue Application
-oc apply -f openshift-tasks-cm-blue.yaml -n ${PROJECT}
+oc apply -f openshift-tasks-blue-config.yaml -n ${PROJECT}
 oc apply -f openshift-tasks-dc-blue.yaml -n ${PROJECT}
 oc apply -f openshift-tasks-svc-blue.yaml -n ${PROJECT}
 
 # Set up Green Application
-oc apply -f openshift-tasks-cm-green.yaml -n ${PROJECT}
+oc apply -f openshift-tasks-green-config.yaml -n ${PROJECT}
 oc apply -f openshift-tasks-dc-green.yaml -n ${PROJECT}
 oc apply -f openshift-tasks-svc-green.yaml -n ${PROJECT}
 
